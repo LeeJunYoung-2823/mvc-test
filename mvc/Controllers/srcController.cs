9919091fb9d760cc7c082b614e48c4b58dcce27d
leejy <l2983@naver.com>
2019-12-26 16:34:23 +0900

    테스트 커밋

---
+++ mvc/Controllers/srcController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+    public class srcController : Controller
+    {
+        public ActionResult index(DateTime? start, DateTime? end, int? page)
+        {
+            if (!start.HasValue && !end.HasValue)
+            {
+                ViewBag.StartDate = DateTime.Parse("2019-06-01");
+                ViewBag.EndDate = DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd") + " 23:59:59.000");
+            }
+            else
+            {
+                ViewBag.StartDate = start;
+                ViewBag.EndDate = end;
+                ViewBag.EndDate = DateTime.Parse(ViewBag.EndDate.ToString("yyyy-MM-dd") + " 23:59:59.000");
+            }
+            
+            if (!page.HasValue) { ViewBag.PageNumber = 1; }
+            else { ViewBag.PageNumber = (page <= 1 ? 1 : page); }
+
+            return View();
+        }
+
+        public ActionResult test()
+        {
+            return View();
+        }
+    }
+}
